835f089fc0479662eef2654d570e10bb0e540276
BDscientist <47311856+BDscientist@users.noreply.github.com>
2019-03-18 16:41:23 +0900

    Add files via upload

--- split_file.py
+++ split_file.py
@@ -6,13 +6,17 @@ sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
 
-part = os.listdir('C:/project1/nsynth-test.jsonwav/nsynth-test')
+#part = os.listdir('C:/project1/nsynth-test.jsonwav/nsynth-test') #part :C:/project1/nsynth-test.jsonwav/nsynth-test
 def gather_information(part):
      # part 인자에는 'train', 'valid', 'test' 등을 넣어줍니다.
      label_list = open('C:/project1/labels.txt').read().strip().split('\n') # labels.txt 파일을 읽어서 각 줄을 기준으로 나눕니다.
      files = [] # wav 파일의 목록
      labels = [] # 각 파일의 label
-     all_files = os.listdir('C:/project1/nsynth-test.jsonwav/nsynth-test/audio') # 각 part 아래 'audio' 안에 있는 파일의 목록을 불러옵니다.
+     all_files = os.listdir('C:/project1/'+part+'/audio') # 각 part 아래 'audio' 안에 있는 파일의 목록을 불러옵니다.
+     #for i in enumerate(all_files,1):
+         #print(i)
+         #print("\n")
+
      for f in all_files: # all_files에 있는 각 파일들에 대해서
         if f[-4:] == '.wav': # 파일이 '.wav'로 끝나면
             files.append(f[:-4]) # files 목록에 추가하고,
@@ -20,17 +24,18 @@ def gather_information(part):
             if label == 'synth': # synth_lead의 경우는 두 단어로 이루어져 있으므로
                 label = 'synth_lead' # 첫 단어가 synth인 경우에는 label 이름을 synth_lead로 바꿔줍니다.
             labels.append(label_list.index(label)) # 이제 label_list에서 label의 위치를 찾아서 labels에 추가합니다.
-        file_out = open(part + '_samples.txt', 'w') # part+'_samples.txt' 에 파일 목록을 적어줍니다.
+        file_out = open('C:/project1/'+part + '_samples.txt', 'w') # part+'_samples.txt' 에 파일 목록을 적어줍니다.
         for f in files:
             file_out.write(f + '\n')
         file_out.close()
-        label_out = open(part + '_labels.txt', 'w') # label도 저장합니다.
+        label_out = open('C:/project1/'+part + '_labels.txt', 'w') # label도 저장합니다.
         for l in labels:
             label_out.write(str(l) + '\n')
         label_out.close()
 
 if __name__ == '__main__' :
     #part = os.listdir('C:/project1/nsynth-test.jsonwav/nsynth-test')
-    gather_information('train') # 위 함수를 'train', 'valid', 'test'에 대해 실행합니다.
-  #gather_information('valid')
-  #gather_information('test')
+     # 위 함수를 'train', 'valid', 'test'에 대해 실행합니다.
+    gather_information('valid')
+    gather_information('test')
+    gather_information('train')
--- test3.py
+++ test3.py
@@ -8,34 +8,34 @@ import io
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
+
 sequence_length = 251
 feature_dimension = 513
 
 def extract_spectrum(part):
-  sample_files = open(part + '_samples.txt').read().strip().split('\n') # 샘플 목록을 읽어옵니다.
-  if part == 'train': # 'train'인 경우에는 평균과 표준편차를 구해야 합니다.
-    data_sum = numpy.zeros((sequence_length, feature_dimension)) # 합계를 저장할 변수를 만듭니다.
-    data_squared_sum = numpy.zeros((sequence_length, feature_dimension)) # 제곱의 합을 저장할 변수입니다.
-  if not os.path.exists(part+'/spectrum/'): # 'spectrum' 디렉토리가 존재하지 않으면 만들어 줍니다.
-    os.mkdir(part+'/spectrum/')
-  for f in sample_files:
-    print('%d/%d: %s'%(sample_files.index(f), len(sample_files), f)) # 현재 진행상황을 출력합니다.
-    y, sr = librosa.load(part+'/audio/'+f+'.wav', sr=16000) # librosa를 이용해 샘플 파일을 읽습니다.
-    D = librosa.stft(y, n_fft=1024, hop_length=256).T # short-time Fourier transform을 합니다.
-    mag, phase = librosa.magphase(D) # phase 정보를 제외하고, 세기만 얻습니다.
-    S = numpy.log(1 + mag * 1000) # 로그형태로 변환합니다.
+    sample_files = open('C:/project1/'+part + '_samples.txt').read().strip().split('\n') # 샘플 목록을 읽어옵니다.
+    if part == 'train': # 'train'인 경우에는 평균과 표준편차를 구해야 합니다.
+        data_sum = numpy.zeros((sequence_length, feature_dimension)) # 합계를 저장할 변수를 만듭니다.
+        data_squared_sum = numpy.zeros((sequence_length, feature_dimension)) # 제곱의 합을 저장할 변수입니다.
+    if not os.path.exists('C:/project1'+part+'/spectrum/'): # 'spectrum' 디렉토리가 존재하지 않으면 만들어 줍니다.
+        os.mkdir('C:/project1'+part+'/spectrum/')
+    for f in sample_files:
+        print('%d/%d: %s'%(sample_files.index(f), len(sample_files), f)) # 현재 진행상황을 출력합니다.
+        y, sr = librosa.load('C:/project1'+part+'/audio/'+f+'.wav', sr=16000) # librosa를 이용해 샘플 파일을 읽습니다.
+        D = librosa.stft(y, n_fft=1024, hop_length=256).T # short-time Fourier transform을 합니다.
+        mag, phase = librosa.magphase(D) # phase 정보를 제외하고, 세기만 얻습니다.
+        S = numpy.log(1 + mag * 1000) # 로그형태로 변환합니다.
     if part == 'train': # 'train'인 경우 합계와 제곱의 합을 누적합니다.
-      data_sum += S
-      data_squared_sum += S ** 2
-    numpy.save(part+'/spectrum/'+f+'.npy', S) # 현재 샘플의 스펙트럼을 저장합니다.
-  if part == 'train': # 모든 파일의 변환이 끝난 후에, 'train'인 경우 평균과 표준편차를 저장합니다.
-    data_mean = data_sum / len(sample_files)
-    data_std = (data_squared_sum / len(sample_files) - data_mean ** 2) ** 0.5
-    numpy.save('data_mean.npy', data_mean)
-    numpy.save('data_std.npy', data_std)
-
+        data_sum += S
+        data_squared_sum += S ** 2
+        numpy.save('C:/project1'+part+'/spectrum/'+f+'.npy', S) # 현재 샘플의 스펙트럼을 저장합니다.
+    if part == 'train': # 모든 파일의 변환이 끝난 후에, 'train'인 경우 평균과 표준편차를 저장합니다.
+        data_mean = data_sum / len(sample_files)
+        data_std = (data_squared_sum / len(sample_files) - data_mean ** 2) ** 0.5
+        numpy.save('data_mean.npy', data_mean)
+        numpy.save('data_std.npy', data_std)
 
 if __name__ == '__main__':
-    part = 'C:\cleanfile\sample.wav'
+
     for part in ['train', 'valid', 'test']:
         extract_spectrum(part)
